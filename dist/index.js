31d69a573c79dd15e67e4eff300238f8b6995526
Abhinav Gautam <abhinav.nanhey@gmail.com>
2024-04-09 18:33:52 +0530

    added files

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+const { KEYVAULT_URL, ENV_KEY } = process.env;
+const credential = new DefaultAzureCredential();
+if (!KEYVAULT_URL || !ENV_KEY)
+    throw new Error("Required Key Vault Variables Missing");
+const client = new SecretClient(KEYVAULT_URL, credential);
+const secret = await client.getSecret(ENV_KEY);
+const secretValue = secret.value;
+if (!secretValue)
+    throw new Error("Value Not Found");
+const parsedSecretValue = JSON.parse(secretValue);
+process.env = { ...process.env, ...parsedSecretValue };
