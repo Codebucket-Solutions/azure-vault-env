02de6c1ad0fadd02ed5b9803fa63621b03f5bf91
Abhinav Gautam <abhinav.nanhey@gmail.com>
2024-04-11 17:11:17 +0530

    try catch and package name

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,18 @@
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 const { KEYVAULT_URL, ENV_KEY } = process.env;
-const credential = new DefaultAzureCredential();
-if (!KEYVAULT_URL || !ENV_KEY)
-    throw new Error("Required Key Vault Variables Missing");
-const client = new SecretClient(KEYVAULT_URL, credential);
-const secret = await client.getSecret(ENV_KEY);
-const secretValue = secret.value;
-if (!secretValue)
-    throw new Error("Value Not Found");
-const parsedSecretValue = JSON.parse(secretValue);
-process.env = { ...process.env, ...parsedSecretValue };
+try {
+    const credential = new DefaultAzureCredential();
+    if (!KEYVAULT_URL || !ENV_KEY)
+        throw new Error("Required Key Vault Variables Missing");
+    const client = new SecretClient(KEYVAULT_URL, credential);
+    const secret = await client.getSecret(ENV_KEY);
+    const secretValue = secret.value;
+    if (!secretValue)
+        throw new Error("Value Not Found");
+    const parsedSecretValue = JSON.parse(secretValue);
+    process.env = { ...process.env, ...parsedSecretValue };
+}
+catch (e) {
+    console.log(e);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@ import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 const { KEYVAULT_URL, ENV_KEY } = process.env;
 
-const credential = new DefaultAzureCredential();
+try {
+  const credential = new DefaultAzureCredential();
 
-if (!KEYVAULT_URL || !ENV_KEY)
-  throw new Error("Required Key Vault Variables Missing");
+  if (!KEYVAULT_URL || !ENV_KEY)
+    throw new Error("Required Key Vault Variables Missing");
 
-const client = new SecretClient(KEYVAULT_URL, credential);
+  const client = new SecretClient(KEYVAULT_URL, credential);
 
-const secret = await client.getSecret(ENV_KEY);
-const secretValue = secret.value;
+  const secret = await client.getSecret(ENV_KEY);
+  const secretValue = secret.value;
 
-if (!secretValue) throw new Error("Value Not Found");
-const parsedSecretValue = JSON.parse(secretValue);
-process.env = { ...process.env, ...parsedSecretValue };
+  if (!secretValue) throw new Error("Value Not Found");
+  const parsedSecretValue = JSON.parse(secretValue);
+  process.env = { ...process.env, ...parsedSecretValue };
+} catch (e) {
+  console.log(e);
+}
